4c0c18e582bae2f09de0ac796cf63289235aaf91
Paul Carmody <pcarmody05@gmail.com>
2014-10-27 06:08:04 -0700

    Monday morning

--- src/coltsequentialcsv.h
+++ src/coltsequentialcsv.h
@@ -17,27 +17,10 @@ public:
 	colt_sequential_csv(char *file_name, int pl=1);
 	virtual ~colt_sequential_csv();
 
-//	virtual colt_base *copy(colt_base *op);
-
-//	void set_quote_char(char q) { quote_char = q; };
-//	void set_sep_char(char s) { column_sep_char = s; };
-//	char sep_char();
-//	int get_next_index(int curr_index, int size, char *buff);
 	int open_file(int set_sep_chars=0);
-//	int load_headers();
-//	int preload_data();
-//	char *extract_str(char *in, char sep_char='\t', char eol_char='\n', char quote_char = '\0');
 	char *record(int line_num);
-//	char *col_header(int n);
 	int num_lines();
-//	int num_cols();
 	int max_size();
-//	char *field_val(int rec_num, int col_num);
-//	char **fields(int rec_num);
-//	bool sort_func(int i, int j);
-//	int show_status(int indent_level=0);
-//
-//	char *source_file_name();
 
 	int get_next_row();
 	int preprocess();
